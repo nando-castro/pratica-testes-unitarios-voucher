2d762b120ce3ed783733a672341e3a557f132643
nando-castro <fernando.castro10.master@gmail.com>
2022-09-22 20:20:39 -0300

    create test voucher duplicated

--- tests/unit/voucher.test.ts
+++ tests/unit/voucher.test.ts
@@ -5,6 +5,7 @@ import voucherService, {
 } from "../../src/services/voucherService";
 
 import voucherRepository from "../../src/repositories/voucherRepository";
+import { rejects } from "assert";
 
 jest.mock("./../../src/repositories/voucherRepository");
 
@@ -20,6 +21,20 @@ describe("voucherService test suite", () => {
     await voucherService.createVoucher("aaa", 10);
     expect(voucherRepository.getVoucherByCode).toBeCalled();
   });
+  it("should test duplicate voucher", async () => {
+    const voucher: VoucherCreateData = {
+      code: "AAA",
+      discount: 10
+    };
 
-  it*
+    jest.spyOn(voucherRepository, "getVoucherByCode").mockImplementationOnce((): any => {
+      return {
+        code: voucher.code,
+        discount: voucher.discount
+      }
+    });
+
+    const promise = voucherService.createVoucher(voucher.code, voucher.discount);
+    expect(promise).rejects.toEqual({ message: "Voucher already exist.", type: "conflict" });
+  });
 });
